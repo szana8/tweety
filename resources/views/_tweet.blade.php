357622cfe49610739a54078d7cd5053c0190d98c
root <szana8@gmail.com>
2020-11-16 12:21:13 +0000

    Add laradock to the project

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,6 +1,6 @@
 <div class="flex p-4 border-b border-grey-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50?u={{ $tweet->user->email }}" alt="" class="rounded-full mr-2">
+        <img src="{{ $tweet->user->avatar }}" alt="" class="rounded-full mr-2">
     </div>
 
     <div>
